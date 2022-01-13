05887c287738e89aa89ff22915097f2aa15b936c
Xiphe <github@xiphe.net>
2022-01-13 11:18:24 +0100

    feat: update öffnungszeiten und address

--- _next/static/iCwXVO971FCOG7DB8syhe/pages/impressum.js
+++ _next/static/iCwXVO971FCOG7DB8syhe/pages/impressum.js
@@ -1,1 +1,280 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["f807"],{F1M5:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return u});var r=t("q1tI"),l=t.n(r),a=t("bgFo"),i=t("YUGZ");function u(){return l.a.createElement(a.b,null,l.a.createElement(i.b,{small:!0,initialLevel:2,title:"Impressum und Haftungsausschluss"}),l.a.createElement(i.c,null,l.a.createElement(a.f,null,l.a.createElement(a.d,{level:1},"Impressum und Haftungsausschluss")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:2},"Verantwortlicher"),l.a.createElement(a.h,null,"Alle Angaben gemäß § 5 TMG",l.a.createElement("br",null),l.a.createElement("strong",null,"Thomas Grossmann"),l.a.createElement("br",null),"Hallerplatz 4",l.a.createElement("br",null),"20146 Hamburg",l.a.createElement("br",null))),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Kontakt"),l.a.createElement(a.h,null,l.a.createElement("strong",null,"Telefon:")," ",l.a.createElement(a.e,{href:"tel:00494085373395"},"+49 (0) 40 85 37 33 95"),l.a.createElement("br",null),l.a.createElement("strong",null,"E-Mail:")," ",l.a.createElement(a.e,{href:"mailto:info@institut-grossmann.com"},"info@institut-grossmann.com"))),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Umsatzsteuer"),l.a.createElement(a.h,null,"Umsatzsteuer-ID-Nummer: entfällt gem. § 4 UStG")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Aufsichtsbehörde"),l.a.createElement(a.h,null,l.a.createElement(a.e,{href:"https://www.hamburg.de/behoerdenfinder/hamburg/11252922/?order=distance&mi%2421_gdb06=hallerplatz+4"},"Bezirksamt Eimsbüttel – Fachamt Gesundheit – Infektionsschutz und Kommunalhygiene"),l.a.createElement("br",null),"Grindelberg 62-66",l.a.createElement("br",null),"20144 Hamburg")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Berufsbezeichnung und berufsrechtliche Regelungen"),l.a.createElement(a.h,null,"Gesetzliche Berufsbezeichnung: Heilpraktiker",l.a.createElement("br",null),"Zuständige Kammer: entfällt",l.a.createElement("br",null),"Verliehen durch: Hamburg",l.a.createElement("br",null),"Es gelten folgende berufsrechtliche Regelungen: Berufsordnung für Heilpraktiker, Regelungen einsehbar unter:"," ",l.a.createElement(a.e,{href:"http://bdhn.de/berufsordnung.html"},"http://bdhn.de/berufsordnung.html"),l.a.createElement("br",null))),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Angaben zur Berufshaftpflichtversicherung"),l.a.createElement(a.h,null,"Name und Sitz des Versicherers:",l.a.createElement("br",null),"Continentale",l.a.createElement("br",null),"Ruhrallee 92",l.a.createElement("br",null),"44139 Dortmund",l.a.createElement("br",null),"Geltungsraum der Versicherung: Deutschland",l.a.createElement("br",null))),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Streitschlichtung"),l.a.createElement(a.h,null,"Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:"," ",l.a.createElement(a.e,{href:"https://ec.europa.eu/consumers/odr"},"https://ec.europa.eu/consumers/odr"),". Unsere E-Mail-Adresse finden Sie oben im Impressum. Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Quellenangaben für Bilder und Grafiken"),l.a.createElement(a.h,null,"Andreas Weiß,"," ",l.a.createElement(a.e,{href:"www.andreasweiss.org"},"www.andreasweiss.org"))),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:1,as:2},"Haftungsausschluss (Disclaimer)")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Haftung für Inhalte"),l.a.createElement(a.h,null,"Die Inhalte unserer Seiten wurden mit größter Sorgfalt nach dem"," ",l.a.createElement(a.e,{href:"http://www.gesetze-im-internet.de/heilprg/BJNR002510939.html"},"Heilpraktikergesetz")," ","sowie der"," ",l.a.createElement(a.e,{href:"http://bdhn.de/berufsordnung.html"},"Berufsordnung für Heilpraktiker")," ","erstellt. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Haftung für Links"),l.a.createElement(a.h,null,"Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Urheberrecht"),l.a.createElement(a.h,null,"Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch.")),l.a.createElement(a.f,null,l.a.createElement(a.d,{level:3},"Heilversprechen und Heilmittelwerbegesetz"),l.a.createElement(a.h,null,"Wir weisen aus rechtlichen Gründen ganz besonders darauf hin, das bei keiner der aufgeführten Leistungen des Institut Thomas Grossmann der Eindruck erweckt wird, dass hier ein Heilungsversprechen zugrunde liegt bzw. Linderung oder Verbesserung eines Krankheitszustandes garantiert oder versprochen wird."))),l.a.createElement(i.a,null))}},RfQF:function(e,n,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/impressum",function(){var e=t("F1M5");return{page:e.default||e}}])}},[["RfQF","5d41","9da1"]]]);
+(window.webpackJsonp = window.webpackJsonp || []).push([
+  ["f807"],
+  {
+    F1M5: function (e, n, t) {
+      "use strict";
+      t.r(n),
+        t.d(n, "default", function () {
+          return u;
+        });
+      var r = t("q1tI"),
+        l = t.n(r),
+        a = t("bgFo"),
+        i = t("YUGZ");
+      function u() {
+        return l.a.createElement(
+          a.b,
+          null,
+          l.a.createElement(i.b, {
+            small: !0,
+            initialLevel: 2,
+            title: "Impressum und Haftungsausschluss",
+          }),
+          l.a.createElement(
+            i.c,
+            null,
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(
+                a.d,
+                { level: 1 },
+                "Impressum und Haftungsausschluss"
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 2 }, "Verantwortlicher"),
+              l.a.createElement(
+                a.h,
+                null,
+                "Alle Angaben gemäß § 5 TMG",
+                l.a.createElement("br", null),
+                l.a.createElement("strong", null, "Thomas Grossmann"),
+                l.a.createElement("br", null),
+                "Hallerplatz 15",
+                l.a.createElement("br", null),
+                "20146 Hamburg",
+                l.a.createElement("br", null)
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 3 }, "Kontakt"),
+              l.a.createElement(
+                a.h,
+                null,
+                l.a.createElement("strong", null, "Telefon:"),
+                " ",
+                l.a.createElement(
+                  a.e,
+                  { href: "tel:00494085373395" },
+                  "+49 (0) 40 85 37 33 95"
+                ),
+                l.a.createElement("br", null),
+                l.a.createElement("strong", null, "E-Mail:"),
+                " ",
+                l.a.createElement(
+                  a.e,
+                  { href: "mailto:info@institut-grossmann.com" },
+                  "info@institut-grossmann.com"
+                )
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 3 }, "Umsatzsteuer"),
+              l.a.createElement(
+                a.h,
+                null,
+                "Umsatzsteuer-ID-Nummer: entfällt gem. § 4 UStG"
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 3 }, "Aufsichtsbehörde"),
+              l.a.createElement(
+                a.h,
+                null,
+                l.a.createElement(
+                  a.e,
+                  {
+                    href: "https://www.hamburg.de/behoerdenfinder/hamburg/11252922/?order=distance&mi%2421_gdb06=hallerplatz+4",
+                  },
+                  "Bezirksamt Eimsbüttel – Fachamt Gesundheit – Infektionsschutz und Kommunalhygiene"
+                ),
+                l.a.createElement("br", null),
+                "Grindelberg 62-66",
+                l.a.createElement("br", null),
+                "20144 Hamburg"
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(
+                a.d,
+                { level: 3 },
+                "Berufsbezeichnung und berufsrechtliche Regelungen"
+              ),
+              l.a.createElement(
+                a.h,
+                null,
+                "Gesetzliche Berufsbezeichnung: Heilpraktiker",
+                l.a.createElement("br", null),
+                "Zuständige Kammer: entfällt",
+                l.a.createElement("br", null),
+                "Verliehen durch: Hamburg",
+                l.a.createElement("br", null),
+                "Es gelten folgende berufsrechtliche Regelungen: Berufsordnung für Heilpraktiker, Regelungen einsehbar unter:",
+                " ",
+                l.a.createElement(
+                  a.e,
+                  { href: "http://bdhn.de/berufsordnung.html" },
+                  "http://bdhn.de/berufsordnung.html"
+                ),
+                l.a.createElement("br", null)
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(
+                a.d,
+                { level: 3 },
+                "Angaben zur Berufshaftpflichtversicherung"
+              ),
+              l.a.createElement(
+                a.h,
+                null,
+                "Name und Sitz des Versicherers:",
+                l.a.createElement("br", null),
+                "Continentale",
+                l.a.createElement("br", null),
+                "Ruhrallee 92",
+                l.a.createElement("br", null),
+                "44139 Dortmund",
+                l.a.createElement("br", null),
+                "Geltungsraum der Versicherung: Deutschland",
+                l.a.createElement("br", null)
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 3 }, "Streitschlichtung"),
+              l.a.createElement(
+                a.h,
+                null,
+                "Die Europäische Kommission stellt eine Plattform zur Online-Streitbeilegung (OS) bereit:",
+                " ",
+                l.a.createElement(
+                  a.e,
+                  { href: "https://ec.europa.eu/consumers/odr" },
+                  "https://ec.europa.eu/consumers/odr"
+                ),
+                ". Unsere E-Mail-Adresse finden Sie oben im Impressum. Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen."
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(
+                a.d,
+                { level: 3 },
+                "Quellenangaben für Bilder und Grafiken"
+              ),
+              l.a.createElement(
+                a.h,
+                null,
+                "Andreas Weiß,",
+                " ",
+                l.a.createElement(
+                  a.e,
+                  { href: "www.andreasweiss.org" },
+                  "www.andreasweiss.org"
+                )
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(
+                a.d,
+                { level: 1, as: 2 },
+                "Haftungsausschluss (Disclaimer)"
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 3 }, "Haftung für Inhalte"),
+              l.a.createElement(
+                a.h,
+                null,
+                "Die Inhalte unserer Seiten wurden mit größter Sorgfalt nach dem",
+                " ",
+                l.a.createElement(
+                  a.e,
+                  {
+                    href: "http://www.gesetze-im-internet.de/heilprg/BJNR002510939.html",
+                  },
+                  "Heilpraktikergesetz"
+                ),
+                " ",
+                "sowie der",
+                " ",
+                l.a.createElement(
+                  a.e,
+                  { href: "http://bdhn.de/berufsordnung.html" },
+                  "Berufsordnung für Heilpraktiker"
+                ),
+                " ",
+                "erstellt. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen."
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 3 }, "Haftung für Links"),
+              l.a.createElement(
+                a.h,
+                null,
+                "Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen."
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(a.d, { level: 3 }, "Urheberrecht"),
+              l.a.createElement(
+                a.h,
+                null,
+                "Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch."
+              )
+            ),
+            l.a.createElement(
+              a.f,
+              null,
+              l.a.createElement(
+                a.d,
+                { level: 3 },
+                "Heilversprechen und Heilmittelwerbegesetz"
+              ),
+              l.a.createElement(
+                a.h,
+                null,
+                "Wir weisen aus rechtlichen Gründen ganz besonders darauf hin, das bei keiner der aufgeführten Leistungen des Institut Thomas Grossmann der Eindruck erweckt wird, dass hier ein Heilungsversprechen zugrunde liegt bzw. Linderung oder Verbesserung eines Krankheitszustandes garantiert oder versprochen wird."
+              )
+            )
+          ),
+          l.a.createElement(i.a, null)
+        );
+      }
+    },
+    RfQF: function (e, n, t) {
+      (window.__NEXT_P = window.__NEXT_P || []).push([
+        "/impressum",
+        function () {
+          var e = t("F1M5");
+          return { page: e.default || e };
+        },
+      ]);
+    },
+  },
+  [["RfQF", "5d41", "9da1"]],
+]);
